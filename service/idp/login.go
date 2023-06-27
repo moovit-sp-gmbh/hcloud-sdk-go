65e6a765aa6dfa9c12f0d2d60d9fb1c7ddd0a55b
Hendrik Vosskamp <h.vosskamp@moovit.de>
2023-06-27 09:49:41 +0200

    Return user objects response on login
    * In addition to the token, also return the response containing the
      entire user object when logging in.

--- service/idp/login.go
+++ service/idp/login.go
@@ -1,16 +1,18 @@
 package idp
 
 import (
+	"net/http"
+
 	"github.com/moovit-sp-gmbh/hcloud-sdk-go"
 	"github.com/moovit-sp-gmbh/hcloud-sdk-go/entities"
 )
 
-func (c *Client) Login(login *entities.Login) (*entities.Token, *hcloud.ErrorResponse) {
+func (c *Client) Login(login *entities.Login) (*entities.Token, *http.Response,*hcloud.ErrorResponse) {
 	resp, _, err := c.client.Post(c.getEndpoint()+"/v1/login", *login)
 	if err != nil {
-		return nil, err
+		return nil, resp, err
 	}
 
 	token := &entities.Token{Token: resp.Header.Get("authorization")}
-	return token, nil
+	return token, resp, nil
 }
