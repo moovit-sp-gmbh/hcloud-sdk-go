17a3f02afe8b061b0d5356728792316d2c3fc7de
Jorge Brown <j.brown@moovit-sp.com>
2024-05-17 15:43:44 +0200

    test: use password instead of token when needed
    This way tests won't break if the token is invalidated.

--- service/idp/user_test.go
+++ service/idp/user_test.go
@@ -10,9 +10,24 @@ import (
 )
 
 var email = os.Getenv("EMAIL")
-var client = &hcloud.Config{
+var config = &hcloud.Config{
 	Server: os.Getenv("SERVER"),
 	Token:  os.Getenv("TOKEN"),
+}
+
+func init() {
+	if config.Token == "" && os.Getenv("PASSWORD") != "" {
+		client := hcloud.New(config)
+		i := New(client)
+		token, _, err := i.Login(&entities.Login{
+			Email:    email,
+			Password: os.Getenv("PASSWORD"),
+		})
+		if err != nil {
+			panic(err)
+		}
+		config.Token = token.Token
+	}
 }
 
 func setupSuite(t *testing.T) {
@@ -20,11 +35,11 @@ func setupSuite(t *testing.T) {
 		t.Errorf("missing EMAIL env")
 		os.Exit(1)
 	}
-	if client.Server == "" {
+	if config.Server == "" {
 		t.Errorf("missing SERVER env")
 		os.Exit(1)
 	}
-	if client.Token == "" {
+	if config.Token == "" {
 		t.Errorf("missing TOKEN env")
 		os.Exit(1)
 	}
@@ -33,7 +48,7 @@ func setupSuite(t *testing.T) {
 func TestIGetUserEmail(t *testing.T) {
 	setupSuite(t)
 
-	client := hcloud.New(client)
+	client := hcloud.New(config)
 	i := New(client)
 	userEmail, _, err := i.GetUserEmail()
 
@@ -49,7 +64,7 @@ func TestIGetUserEmail(t *testing.T) {
 func TestIGetUser(t *testing.T) {
 	setupSuite(t)
 
-	client := hcloud.New(client)
+	client := hcloud.New(config)
 	i := New(client)
 	user, _, err := i.GetUser()
 
@@ -65,7 +80,7 @@ func TestIGetUser(t *testing.T) {
 func TestIGetUserNats(t *testing.T) {
 	setupSuite(t)
 
-	client := hcloud.New(client)
+	client := hcloud.New(config)
 	i := New(client)
 	userNats, _, err := i.GetUserNats()
 
@@ -87,7 +102,7 @@ func TestIPatchUser(t *testing.T) {
 	setupSuite(t)
 	t.Skip("Skipping testing as backend not ready")
 
-	client := hcloud.New(client)
+	client := hcloud.New(config)
 	i := New(client)
 	randStringBytes := randStringBytes(12)
 	patchedUser, _, err := i.PatchUser(&entities.PatchUser{Company: randStringBytes})
@@ -108,7 +123,7 @@ func TestIPatchUser(t *testing.T) {
 func TestIGetUserOrgs(t *testing.T) {
 	setupSuite(t)
 
-	client := hcloud.New(client)
+	client := hcloud.New(config)
 	i := New(client)
 	userOrgs, total, _, err := i.GetUserOrganizations(1, 0, entities.Search{})
 
@@ -129,7 +144,7 @@ func TestISearchUserOrgs(t *testing.T) {
 	setupSuite(t)
 	t.Skip("Skipping testing as backend not ready")
 
-	client := hcloud.New(client)
+	client := hcloud.New(config)
 	i := New(client)
 	userOrgs, total, _, err := i.SearchUserOrganizations(&entities.SearchOrganizationFilter{}, 1, 0)
 
